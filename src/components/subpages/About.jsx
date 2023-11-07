3f68142d025e29ad1cf829419d15e5e89c77b3f4
MariuszBednarz <mmbednarz90@gmail.com>
2023-11-07 13:03:51 +0100

    hooks examples

--- src/components/subpages/About.jsx
+++ src/components/subpages/About.jsx
@@ -1,7 +1,5 @@
+import { useReducer } from "react";
 import styled from "styled-components";
-import { useContext } from "react";
-
-import { StyleContext } from "../../App";
 
 const Wrapper = styled.div`
   display: flex;
@@ -10,41 +8,61 @@ const Wrapper = styled.div`
   p {
     text-align: center;
   }
-  background-color: ${(props) => (props.darkTheme ? "black" : "white")};
-  color: ${(props) => (props.darkTheme ? "white" : "black")};
-`;
-
-const Title = styled.h1`
-  font-size: 36px;
-  text-align: center;
 `;
 
 const About = () => {
-  const darkTheme = useContext(StyleContext);
+  const [state, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case "increment":
+          return { ...state, count: state.count + 1 };
+        case "decrement":
+          return { ...state, count: state.count - 1 };
+        case "reset":
+          return { ...state, count: 0 };
+        case "change_input":
+          return { ...state, inputValue: action.value };
+        default:
+          throw new Error();
+      }
+    },
+    { count: 0, inputValue: "" }
+  );
+
+  const handleInputChange = (e) => {
+    dispatch({ type: "change_input", value: e.target.value });
+  };
+
+  console.log(state);
 
   return (
-    <Wrapper darkTheme={darkTheme}>
-      <Title>About</Title>
-
-      {darkTheme ? <h1>Tryb Ciemny</h1> : <h1>Tryb Jasny</h1>}
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quia
-        fugiat repellendus maxime soluta iusto quod distinctio possimus
-        asperiores voluptas. Veniam nulla ad perferendis explicabo magnam unde
-        aperiam labore in incidunt exercitationem, ex quo libero amet,
-        distinctio quia, odit suscipit harum delectus. Quasi ipsam hic
-        doloremque officiis eos non animi!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quia
-        fugiat repellendus maxime soluta iusto quod distinctio possimus
-        asperiores voluptas. Veniam nulla ad perferendis explicabo magnam unde
-        aperiam labore in incidunt exercitationem, ex quo libero amet,
-        distinctio quia, odit suscipit harum delectus. Quasi ipsam hic
-        doloremque officiis eos non animi! Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Voluptates adipisci blanditiis praesentium
-        debitis cumque libero fuga ullam animi sint eligendi.
-      </p>
+    <Wrapper>
+      <div>
+        Licznik: {state.count}
+        <button
+          className="w-8 h-8 m-4 bg-slate-400"
+          onClick={() => dispatch({ type: "increment" })}
+        >
+          +
+        </button>
+        <button
+          className="w-8 h-8 m-4 bg-slate-400"
+          onClick={() => dispatch({ type: "decrement" })}
+        >
+          -
+        </button>
+        <button
+          className="w-16 h-8 m-4 bg-slate-400"
+          onClick={() => dispatch({ type: "reset" })}
+        >
+          reset
+        </button>
+      </div>
+      <input
+        type="text"
+        onChange={handleInputChange}
+        value={state.inputValue}
+      />
     </Wrapper>
   );
 };
--- src/components/subpages/Home.jsx
+++ src/components/subpages/Home.jsx
@@ -1,5 +1,6 @@
+import { useRef, useState, useMemo, useCallback } from "react";
+
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
 const Wrapper = styled.div`
   display: flex;
@@ -10,33 +11,80 @@ const Wrapper = styled.div`
   }
 `;
 
-const Title = styled.h1`
-  font-size: 36px;
-  text-align: center;
-`;
-
-const CustomNav = styled(Title)`
-  border: 1px solid red;
-  padding: 10px;
-`;
-
 const Home = () => {
+  //useRef
+  const inputRef = useRef();
+  const section1 = useRef();
+  const section2 = useRef();
+  const section3 = useRef();
+  const scrollToSection = (section) => {
+    section.current.scrollIntoView({ behavior: "smooth" });
+    console.log(section.current);
+  };
+  const focus = () => {
+    inputRef.current.focus();
+    console.log(inputRef.current);
+    console.log(inputRef.current.value);
+  };
 
+  //useMemo
+  const [count, setCount] = useState(0);
+
+  const [values] = useState(() =>
+    Array.from({ length: 1000000 }, (_, index) => index)
+  );
+
+  const calcLargeArraySum = (array) => {
+    console.log("Obliczanie...");
+    return array.reduce((sum, value) => sum + value, 0);
+  };
+
+  // const sum = calcLargeArraySum(values);
+  const sum = useMemo(() => calcLargeArraySum(values), [values]);
+
+  //useCallback
+  const getSum = useCallback(() => calcLargeArraySum(values), [values]);
+
+  console.log("rerender");
   return (
     <Wrapper>
-      <Title>Home Page</Title>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quia
-        fugiat repellendus maxime soluta iusto quod distinctio possimus
-        asperiores voluptas. Veniam nulla ad perferendis explicabo magnam unde
-        aperiam labore in incidunt exercitationem, ex quo libero amet,
-        distinctio quia, odit suscipit harum delectus. Quasi ipsam hic
-        doloremque officiis eos non animi!
-      </p>
-      <Title>Skontaktuj się z nami</Title>
-      <Link to="/about">
-        <CustomNav>Klikając tutaj!</CustomNav>
-      </Link>
+      {/* useMemo */}
+      <h1>USE MEMO EXAMPLE</h1>
+      <div className="w-full h-40 bg-slate-500 flex">
+        <h2>Suma: {sum}</h2>
+        <button onClick={() => setCount(count + 1)}>Kliknij {count}</button>
+      </div>
+
+      {/* useCallback */}
+      <h1>USE CALLBACK EXAMPLE</h1>
+      <div className="w-full h-40 bg-slate-400 flex">
+        <button onClick={() => console.log(getSum())}>ObliczSumę</button>
+      </div>
+
+      {/* useRef */}
+      <h1>USEREF EXAMPLE</h1>
+      <button onClick={() => scrollToSection(section1)}>
+        przejdź do sekcji 1
+      </button>
+      <button onClick={() => scrollToSection(section2)}>
+        przejdź do sekcji 2
+      </button>
+      <button onClick={() => scrollToSection(section3)}>
+        przejdź do sekcji 3
+      </button>
+
+      <input type="text" ref={inputRef} />
+      <button onClick={focus}>Focus on input</button>
+
+      <div className="h-screen bg-slate-300 text-xl" ref={section1}>
+        Sekcja1
+      </div>
+      <div className="h-screen bg-slate-400 text-xl" ref={section2}>
+        Sekcja2
+      </div>
+      <div className="h-screen bg-slate-300 text-xl" ref={section3}>
+        Sekcja3
+      </div>
     </Wrapper>
   );
 };
