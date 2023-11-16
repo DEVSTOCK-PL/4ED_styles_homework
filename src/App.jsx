54630bcdcce3bc6993091068eb237bb433dd429e
BartoszKrysztofik <bartoszkrysztofik.tech@gmail.com>
2023-11-16 18:54:44 +0100

    kolejne

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
-import SocialProof from './components/SocialProof'
-import CtaOne from './components/CTA_1'
-import CustomerLogos from './components/CustomerLogos'
-import CtaTwo from './components/CTA_2'
-import SectorBlog from './components/Blog'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import { Hero, Navbar, SocialProof, CtaOne, CtaTwo, CustomerLogos, SectorBlog, Footer, Contact}
 
 
 function App() {
---
+++ src/generalComponents/index.js
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import SocialProof from "../components/SocialProof";
+import CtaOne from "../components/CTA_1";
+import CtaTwo from "../components/CTA_2";
+import CustomerLogos from "../components/CustomerLogos";
+import SectorBlog from "../components/Blog";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+
+export { Hero, Navbar, SocialProof, CtaOne, CtaTwo, CustomerLogos, SectorBlog, Footer, Contact}
