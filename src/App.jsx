351b3b66807310d4f9dd651efbb2b0dd0a789c80
MariuszBednarz <mmbednarz90@gmail.com>
2023-11-16 15:07:54 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./GlobalStyleCSS";
+import { ThemeContext } from "./components/hooks/ThemeContext";
+import UserProvider from "./components/hooks/UserContext";
+import { lightTheme, darkTheme } from "./components/themes";
+import { SnackbarProvider } from "notistack";
+import { Router } from "./pages/Router";
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: ${({ theme }) => theme.body};
+
+  & > * {
+    padding-top: 96px;
+    padding-bottom: 96px;
+  }
+
+  & > :first-child {
+    padding-top: 24px;
+    padding-bottom: 0px;
+  }
+
+  & > :last-child {
+    padding-top: 96px;
+    padding-bottom: 64px;
+  }
+`;
+
+export const SpecialCointaner = styled(Container)`
+  padding-top: 0px;
+
+  & > :last-child {
+    padding-top: 0px;
+    padding-bottom: 0px;
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    if (theme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <SnackbarProvider>
+        <ThemeContext.Provider value={toggleTheme}>
+          <UserProvider>
+            <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
+              <GlobalStyle />
+              <Container>
+                <Router />
+              </Container>
+            </ThemeProvider>
+          </UserProvider>
+        </ThemeContext.Provider>
+      </SnackbarProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/themes.js
@@ -0,0 +1,17 @@
+export const lightTheme = {
+  body: "#eee6d7",
+  title: "#000",
+  text: "#655c50",
+  toggleBorder: "#bac8e4",
+  footerBackground: "#e0d5c8",
+  logosText: "#1a56db",
+};
+
+export const darkTheme = {
+  body: "#111928",
+  title: "#fff",
+  text: "#9ca3af",
+  toggleBorder: "#374151",
+  footerBackground: "#1f2a37",
+  logosText: "#6b7280",
+};
